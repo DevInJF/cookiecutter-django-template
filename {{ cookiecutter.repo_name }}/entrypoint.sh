c3b7b3db036ac7ee3df1e454660771f025e8f7b0
André Luiz <contato@xdvl.info>
2016-01-20 23:54:24 -0200

    Usa o gosu assim podemos usar o virtualenv sem problemas
    Pois ele leva em conta o $PATH o sh não.

--- {{ cookiecutter.repo_name }}/entrypoint.sh
+++ {{ cookiecutter.repo_name }}/entrypoint.sh
@@ -3,8 +3,7 @@
 gid=$(stat --format %g /usr/src/app)
 uid=$(stat --format %u /usr/src/app)
 
-groupadd --gid $gid foo
-useradd --gid $gid --uid $uid foo
+groupadd --gid $gid foo 2>/dev/null
+useradd --gid $gid --uid $uid foo 2>/dev/null
 
-# TODO(andre): usar o gosu
-su $(id --user --name $uid) -c "$*"
+./bin/gosu-amd64 $uid:$gid $*
