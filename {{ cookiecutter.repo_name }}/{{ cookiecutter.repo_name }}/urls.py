bd981ee4c3112de86124aff7e8ed011fcef902ba
André Luiz <contato@xdvl.info>
2016-11-19 01:25:09 -0200

    Update urls.py

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/urls.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -6,3 +7,10 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
     url(r'^admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
