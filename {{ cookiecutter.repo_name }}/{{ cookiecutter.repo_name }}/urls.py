38d7c3bd8c23022e25b066c10f7ddbe4fef085c3
André Luiz <contato@xdvl.info>
2017-12-09 18:30:36 -0200

    Update urls.py

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/urls.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls import include, path
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.urls import include, path
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base.html'), name='index'),
